77d7d14ecf534135674ef1c18801306ab4bcdae3
GustavoNappi <gustavosouzanappi03@gmail.com>
2025-09-17 16:42:29 -0300

    Arrays e Funções

---
+++ Arrays/index.js
@@ -0,0 +1,25 @@
+const nome = 'Gustavo Nappi'
+const alunos = ['Gustavo', 'Maria', 'João'];
+
+
+
+
+
+
+
+
+
+// console.log(typeof alunos)
+// console.log(alunos instanceof Array)
+// alunos.push('Luiza')
+// alunos.push('Eduardo')
+//console.log(alunos.slice(0,-2))
+// delete alunos[1];
+// console.log(alunos[1])
+// console.log(alunos);
+// console.log(alunos[0])
+// console.log(alunos[2])
+//alunos.unshift('Luiz');// Adiciona no inicio do array
+//alunos.push('Fábio') //  Adiciona no fim
+//alunos[alunos.length]// Tamanho do array
+// const removido = alunos.pop() // Remove indice do array
---
+++ Funções em JavaScript - Uma introdução ao básico/index.js
@@ -0,0 +1,15 @@
+
+function soma (x ,y){
+    const resultado = x + y;
+    return  resultado
+}
+
+console.log(somc(2,2));
+
+// function saudacao(nome){
+//     console.log(`Bom dia ${nome}`);
+//     return 
+// }
+
+// const variavel = saudacao('Salah')
+// console.log(variavel)
