be49aa16d92bf6d5cf16f6a2af55495aa139be39
GustavoNappi <gustavosouzanappi03@gmail.com>
2025-09-17 18:18:16 -0300

    Criando Objetos

---
+++ Objetos/index.js
@@ -0,0 +1,56 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function criaPessoa(nome, sobrenome, idade){
+//     return{nome,sobrenome,idade};
+// }
+
+
+// const pessoa1 = criaPessoa('Gustavo', 'Nappi', 22)
+// const pessoa2 = criaPessoa('Giovanna', 'Pereira', 22)
+// const pessoa3 = criaPessoa('Yuri', 'Lopes', 21)
+// const pessoa4 = criaPessoa('Dani', 'Nappi', 25)
+// const pessoa5 = criaPessoa('Patrick', 'Oliveira', 21)
+
+
+// console.log(pessoa1,pessoa2,pessoa3,pessoa4, pessoa5)
+
+
+// const pessoa1 = {
+//     nome: 'Gustavo',
+//     sobrenome: 'Nappi',
+//     idade:22
+// };
+
+
+// console.log(pessoa1)
+
+
+// const name01 = 'Gustavo';
+// const lastName1 = 'Miranda';
+// const age01 = 22;
+
+// const name02 = 'Mario';
+// const lastName2 = 'League'
+// const age02 = 34
+
+// const array = [1,2,3];
+// array.push(4);
+// array[0] = 'Luiz'
+// //Isso não é permitido
+// array = 'Outra' 
