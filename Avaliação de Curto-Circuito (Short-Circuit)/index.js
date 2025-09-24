cb1add62a01b1afd3574645bead0f4f20c6578ed
GustavoNappi <gustavosouzanappi03@gmail.com>
2025-09-24 11:32:22 -0300

    Curtos circuitos

---
+++ Avaliação de Curto-Circuito (Short-Circuit)/index.js
@@ -0,0 +1,11 @@
+/*
+&& -> Todas as expressões precisam ser verdaderias
+|| -> Todas precisam ser falsas
+*/
+console.log('Luiz Otávio' && 0 && 'Maria');
+
+function falaOi(){
+    return 'OI'
+}
+
+const vaiExecutar = false
