c986074671e771ac765c2a951fbbaae025e186e1
GustavoNappi <gustavosouzanappi03@gmail.com>
2025-09-24 16:09:02 -0300

    if elese(1)

---
+++ if, else if e else (1)/index.js
@@ -0,0 +1,37 @@
+/*
+
+Entre 0 - 11 - bom dia
+Entre 12 - 17 - boa tarde
+Entre 18 - 23 - Boa noite
+
+*/
+// if pode ser usado sozinho
+// Sempre que utilizo a palavra else, preciso de um if antes
+// Eu posso ter varios else ifs na checagem
+// so posso ter um else na checagem
+// Podemos usar condições sem else if usando apenas if else
+
+ 
+// const hora = 50;
+
+// if (hora >= 0 && hora <= 11){
+//     console.log('Bom dia')
+// } else if(hora >= 12 && hora <= 17) {
+//     console.log('Boa tarde')
+// }else if(hora >= 18 && hora <= 23)
+// {
+//     console.log('Boa noite')
+// } else {
+//     console.log('Hora invalida')
+// }
+
+
+
+// const tenhoGrana = true;
+
+// if (tenhoGrana){
+//     console.log('Vou sair de casa');
+
+// }else {
+//     console.log('Não vou sair nao')
+// }
