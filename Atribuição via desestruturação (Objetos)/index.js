bd1ea770ce817a5d8bbe77df578b32a7c54c6f7d
Gustavo Souza Nappi <gustavo.nappi@petronect.com.br>
2025-10-29 14:23:03 -0300

    For in

---
+++ Atribuição via desestruturação (Objetos)/index.js
@@ -0,0 +1,14 @@
+const pessoa = {
+    nome: 'Luiz',
+    sobrenome: 'Miranda',
+    idade:30,
+    endereço:{
+        rua:'Av Brasil',
+        numero:320
+    }
+};
+
+
+// Atribuição via desestruturação
+const { nome, sobrenome, idade } = pessoa
+console.log(nome, sobrenome);
