2d1689cc625413abb6ae929b79abfa9c464e800d
GustavoNappi <gustavosouzanappi03@gmail.com>
2025-11-05 09:55:21 -0300

    Making the foreach

--- For each estrutura de repetição/index.js
+++ For each estrutura de repetição/index.js
@@ -1,9 +1,16 @@
-const nome = 'Luiz Otávio';
+const nome = ['luiz', 'otavio', 'henrique'];
 
 // for (let i = 0; i < nome.length ; i++ ){
 //     console.log(nome[i])
 // }
 
+
+for (let i in nome ){
+    console.log(nome[i])
+}
+
+
+console.log("#######")
 
 for (let valor of nome){
     console.log(valor)
