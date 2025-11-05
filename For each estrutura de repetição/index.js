e7949e908482e094918456f12b58cfe5fa5e1b89
GustavoNappi <gustavosouzanappi03@gmail.com>
2025-11-05 11:16:56 -0300

    Coments

--- For each estrutura de repetição/index.js
+++ For each estrutura de repetição/index.js
@@ -1,23 +1,43 @@
-const nomes = ['luiz', 'otavio', 'henrique'];
+// const nomes = ['luiz', 'otavio', 'henrique'];
+
+
+// For classico - Geralmente com iteráveis(array ou strings)
+// For in - Retorna o índice ou chave (string, array ou objetos)
+// For of - Retorna o valor em si (iteraveis, arrays ou strings)
+
+
 
 // for (let i = 0; i < nome.length ; i++ ){
 //     console.log(nome[i])
 // }
 
 
-for (let i in nome ){
-    console.log(nome[i])
+
+// for (let i in nome ){
+//     console.log(nome[i])
+// }
+
+
+// console.log("#######")
+
+// for (let valor of nome){
+//     console.log(valor)
+// }
+
+
+
+// nomes.forEach(function(valor, indice, array) {
+//     console.log(valor, indice, array);
+// });
+
+
+
+
+const pessoa = {
+    nome:'Gustavo',
+    sobrenome:'Nappi'
+};
+
+for(let i in pessoa){
+    console.log(i)
 }
-
-
-console.log("#######")
-
-for (let valor of nome){
-    console.log(valor)
-}
-
-
-
-nomes.forEach(function(valor, indice) {
-    console.log(valor, indice);
-});
