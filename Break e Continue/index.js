656b00e5166c1a4b4a5cb95fda56edb24f923cbf
GustavoNappi <gustavosouzanappi03@gmail.com>
2025-11-13 11:48:05 -0300

    BREAK AND CONTINUE

---
+++ Break e Continue/index.js
@@ -0,0 +1,105 @@
+const numeros = [1,2,3,4,5,6,7,8,9];
+
+//CONTINUE CONTINUA PAR A PROXIMA ITERAÇÃO
+//BREAK SAI DO LAÇO
+
+
+
+// for (let numero of numeros){
+//     if (numero === 2){
+//         console.log('Pulei o número 2');
+//         continue
+//     }
+//         console.log(numero)
+
+//     if(numero === 7){
+//         console.log('7 encontrado, saindo...')
+//         break
+//     }
+// }
+
+
+
+
+
+
+// for (let i in numeros){
+//     let numero =  numeros[i]
+
+//     if (numero === 2){
+//         console.log('Pulei o número 2');
+//         continue
+//     }
+//         console.log(numero)
+
+//     if(numero === 7){
+//         console.log('7 encontrado, saindo...')
+//         break
+//     }
+// }
+
+
+// console.log('Com o do while')
+// let i = 0;
+// while (i < numeros.length){
+//     let numero = numeros[i]
+//     if (numero === 2){
+//         console.log('Pulei o número 2');
+//     i++;
+
+//         continue
+//     }
+//         console.log(numero)
+
+//     if(numero === 7){
+//         console.log('7 encontrado, saindo...')
+//     i++;
+
+//         break
+//     }
+//     i++;
+// }
+
+
+// console.log('Com o do while')
+// let i = 0;
+// do{
+//     let numero = numeros[i]
+//     if (numero === 2){
+//         console.log('Pulei o número 2');
+//     i++;
+
+//         continue
+//     }
+//         console.log(numero)
+
+//     if(numero === 7){
+//         console.log('7 encontrado, saindo...')
+//     i++;
+
+//         break
+//     }
+//     i++;
+// }
+
+
+
+console.log('Com o for classico')
+
+for (let i = 0; i < numeros.length; i++){
+    let numero = numeros[i]
+    if (numero === 2){
+        console.log('Pulei o número 2');
+        continue
+    }
+        console.log(numero)
+
+    if(numero === 7){
+        console.log('7 encontrado, saindo...')
+        break
+    }
+}
+
+
+
+
