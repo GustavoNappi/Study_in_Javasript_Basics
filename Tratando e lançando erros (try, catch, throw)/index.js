4c4643fe33bd7c4ac119152c60a8222d2ceff8f9
GustavoNappi <gustavosouzanappi03@gmail.com>
2025-11-25 11:52:33 -0300

    Try catch reference

--- Tratando e lançando erros (try, catch, throw)/index.js
+++ Tratando e lançando erros (try, catch, throw)/index.js
@@ -1,5 +1,24 @@
-try {
-    console.log(naoexiste)
-} catch(e){
-    console.log('nao exsito nao existe.')
+// TRY CATCH NAO É BOM FAZER ISSO DEVE SER MELHOR TRATAR O ERRO
+// try {
+//     console.log(naoexiste)
+// } catch(e){
+//     console.log('nao exsito nao existe.')
+// }
+
+
+function soma(x,y){
+    if(typeof x !== 'number' || 
+        typeof y !== 'number'){
+        throw new ReferenceError('x e y precisam ser numeros')
+    }
+
+    return x + y
+}
+
+
+try { 
+console.log(soma(1,2))
+console.log(soma('1',2))
+} catch(error) {
+    console.log(error)
 }
